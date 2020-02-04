a9ed894868036b2b9ae7185961a4e84c58925f91
soosgabor <malagi@gmail.com>
2020-02-04 13:32:19 +0100

    A feltöltés eljárás meghívása.

--- Aknakereso/Program.cs
+++ Aknakereso/Program.cs
@@ -8,10 +8,13 @@ namespace Aknakereso
 {
     class Program
     {
+        char[,] pálya=new char[10,10];
         static void Main(string[] args)
         {
-            Console.WriteLine("Helló GIT!");
+            Feltöltés();
+
             Console.ReadKey();
         }
+        
     }
 }
