2bf12c64a8712b2c1f7da498aa2dd957ba3c0e10
soosgabor <malagi@gmail.com>
2020-02-04 13:51:26 +0100

    dfg

--- Aknakereso/Program.cs
+++ Aknakereso/Program.cs
@@ -8,11 +8,17 @@ namespace Aknakereso
 {
     class Program
     {
-        char[,] pálya=new char[10,10];
+        
+
+        static void Feltöltés(char[,] pálya){
+
+        }
+
         static void Main(string[] args)
         {
-            Feltöltés();
-
+            char[,] pálya=new char[10,10];
+            Feltöltés(pálya);
+            Kirajzoló();
             Console.ReadKey();
         }
         
